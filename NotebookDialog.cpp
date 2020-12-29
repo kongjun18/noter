0a55a3dbf4cfcd1611093df068fe7a533f119017
Kong Jun <kongjun18@outlook.com>
2020-12-29 17:18:18 +0800

    fix: NotebookDialog can't handle path correctly

--- NotebookDialog.cpp
+++ NotebookDialog.cpp
@@ -1,1 +1,76 @@
 #include "NotebookDialog.h"
+
+std::optional<std::pair<const QString, const QString>>
+NotebookAbstractDialog::getInput()
+{
+    // If user type TAB to move to next line, text() will hold '\t'
+    auto notebook{ m_lineEdit1->text().trimmed() };
+    auto directory{ m_lineEdit2->text().trimmed() };
+    qDebug() << QString("notebook: %1").arg(notebook);
+    qDebug() << QString("directory: %1").arg(directory);
+    if (notebook.isEmpty()) {
+        emit showMessageSignal(tr("Notebook shound not be empty"));
+        m_lineEdit1->setBackgroundRole(QPalette::Highlight);
+        m_lineEdit1->setPalette(QPalette(QColor(Qt::red)));
+        return std::nullopt;
+    }
+    if (directory.isEmpty()) {
+        emit showMessageSignal(tr("Directory shound not be empty"));
+        m_lineEdit2->setBackgroundRole(QPalette::Highlight);
+        m_lineEdit2->setPalette(QPalette(QColor(Qt::red)));
+        return std::nullopt;
+    }
+    // User can input directory like `~`
+    directory.replace(QRegularExpression("^~"), QDir::homePath());
+    return std::make_pair(notebook, directory);
+}
+
+NotebookImportDialog::NotebookImportDialog(QWidget* parent,
+                                           const QString& title,
+                                           const QString& label1,
+                                           const QString& label2,
+                                           const QString& button1,
+                                           const QString& button2)
+  : NotebookAbstractDialog(parent, title, label1, label2, button1, button2)
+{
+    QObject::connect(m_button1, &QPushButton::clicked, [&]() {
+        if (const auto option{ getInput() }; option) {
+            const auto [notebook, directory] = option.value();
+            QDir qdir{ directory };
+            if (!qdir.exists()) {
+                QMessageBox::warning(
+                  this,
+                  tr("Directory no exists"),
+                  tr("Please check directory %1").arg(directory));
+                return;
+            }
+            emit importNotebookSignal(notebook, directory);
+        }
+    });
+    QObject::connect(m_button2, &QPushButton::clicked, [this]() { close(); });
+}
+
+NotebookNewDialog::NotebookNewDialog(QWidget* parent,
+                                        const QString& title,
+                                        const QString& label1,
+                                        const QString& label2,
+                                        const QString& button1,
+                                        const QString& button2)
+  : NotebookAbstractDialog(parent, title, label1, label2, button1, button2)
+{
+    QObject::connect(m_button1, &QPushButton::clicked, [this]() {
+        if (const auto option{ getInput() }; option) {
+            const auto [notebook, directory] = option.value();
+            QDir qdir{ directory };
+            if (qdir.exists()) {
+                QMessageBox::warning(
+                  this,
+                  tr("Directory exists"),
+                  tr("Please check directory %1").arg(directory));
+                return;
+            }
+            emit newNotebookSignal(notebook, directory);
+        }
+    });
+    QObject::connect(m_button2, &QPushButton::clicked, [this]() { close(); });
+}
--- NotebookDialog.h
+++ NotebookDialog.h
@@ -8,11 +8,14 @@
 #include <QLabel>
 #include <QLineEdit>
 #include <QMessageBox>
+#include <QObject>
 #include <QPushButton>
 #include <QRegularExpression>
 #include <QString>
 #include <QVBoxLayout>
 #include <QWidget>
+#include <optional>
+#include <utility>
 class NotebookAbstractDialog : public QWidget
 {
     Q_OBJECT
@@ -22,7 +25,7 @@ class NotebookAbstractDialog : public QWidget
                            const QString& label1,
                            const QString& label2,
                            const QString& button1,
-                           const QString& button2 = tr("Cancel"))
+                           const QString& button2 = tr("Close"))
       : QWidget(parent)
       , m_label1(new QLabel(label1, this))
       , m_label2(new QLabel(label2, this))
@@ -45,12 +48,16 @@ class NotebookAbstractDialog : public QWidget
         setWindowFlag(Qt::Dialog);
     }
 
+  protected:
     QLabel* m_label1;
     QLabel* m_label2;
     QLineEdit* m_lineEdit1;
     QLineEdit* m_lineEdit2;
     QPushButton* m_button1;
     QPushButton* m_button2;
+    std::optional<std::pair<const QString, const QString>> getInput();
+  signals:
+    void showMessageSignal(const QString& message, int timeout = 5000);
 };
 
 class NotebookImportDialog : public NotebookAbstractDialog
@@ -62,52 +69,7 @@ class NotebookImportDialog : public NotebookAbstractDialog
                          const QString& label1 = tr("Notebook"),
                          const QString& label2 = tr("Directory"),
                          const QString& button1 = tr("Import"),
-                         const QString& button2 = tr("Cancel"))
-      : NotebookAbstractDialog(parent, title, label1, label2, button1, button2)
-    {
-        QObject::connect(m_button1, &QPushButton::clicked, [this]() {
-            const auto notebook{ m_lineEdit1->text().trimmed() };
-            // If user type TAB to move to next line, text() will hold '\t'
-            auto directory{ m_lineEdit2->text().trimmed() };
-            // directory with suffix '/' or '\' such as '~/Document/'
-#if defined(Q_OS_UNIX)
-            // if (directory.back() == QChar('/'))
-#elif defined(Q_OS_WIN)
-            // if (directory.back()== QChar('\'))
-#endif
-            // {
-            //     directory.remove(directory.length() - 1, 1);
-            //     // directory is root
-            //     if (directory.isEmpty()) {
-            //         QMessageBox::warning(
-            //           this, tr("Input error"), tr("Please check input "));
-            //         return;
-            //     }
-            // }
-            if (notebook.isEmpty()) {
-                m_lineEdit1->setBackgroundRole(QPalette::Highlight);
-                m_lineEdit1->setPalette(QPalette(QColor(Qt::red)));
-                return;
-            }
-            if (directory.isEmpty()) {
-                m_lineEdit2->setBackgroundRole(QPalette::Window);
-                return;
-            }
-            // User can input directory like `~`
-            directory = QDir::toNativeSeparators(
-              directory.replace(QRegularExpression("^~"), QDir::homePath()));
-            QDir qdir{ directory };
-            if (!qdir.exists()) {
-                QMessageBox::warning(
-                  this,
-                  tr("Directory no exists"),
-                  tr("Please check directory %1").arg(directory));
-                return;
-            }
-            qDebug() << "directory: " << directory;
-            emit importNotebookSignal(notebook, directory);
-        });
-    }
+                         const QString& button2 = tr("Close"));
   signals:
     void importNotebookSignal(const QString& notebook, const QString& path);
 };
@@ -121,54 +83,7 @@ class NotebookNewDialog : public NotebookAbstractDialog
                       const QString& label1 = tr("Notebook"),
                       const QString& label2 = tr("Directory"),
                       const QString& button1 = tr("Create"),
-                      const QString& button2 = tr("Cancel"))
-      : NotebookAbstractDialog(parent, title, label1, label2, button1, button2)
-    {
-        QObject::connect(m_button1, &QPushButton::clicked, [this]() {
-            const auto notebook{ m_lineEdit1->text().trimmed() };
-            // If user type TAB to move to next line, text() will hold '\t'
-            auto directory{ m_lineEdit2->text().trimmed() };
-            // directory with suffix '/' or '\' such as "~/Document/"
-/*
- * #ifdef Q_OS_LINUX
- *             if (directory.back() == QChar('/'))
- * #elif Q_OS_WIN32
- *             if (directory.back()== QChar('\'))
- * #endif
- *             {
- *                 directory.remove(directory.length() - 1, 1);
- *                 // directory is root
- *                 if (directory.isEmpty()) {
- *                     QMessageBox::warning(
- *                       this, tr("Input error"), tr("Please check input "));
- *                     return;
- *                 }
- *             }
- */
-            if (notebook.isEmpty()) {
-                m_lineEdit1->setBackgroundRole(QPalette::Highlight);
-                m_lineEdit1->setPalette(QPalette(QColor(Qt::red)));
-                return;
-            }
-            if (directory.isEmpty()) {
-                m_lineEdit2->setBackgroundRole(QPalette::Window);
-                return;
-            }
-            directory = QDir::toNativeSeparators(
-              directory.replace(QRegularExpression("^~"), QDir::homePath()));
-            QDir qdir{ directory };
-            if (qdir.exists()) {
-                QMessageBox::warning(
-                  this,
-                  tr("Directory exists"),
-                  tr("Please check directory %1").arg(directory));
-                return;
-            }
-            // User can type directory like '~'
-            directory = qdir.absolutePath();
-            emit newNotebookSignal(notebook, directory);
-        });
-    }
+                      const QString& button2 = tr("Close"));
   signals:
     void newNotebookSignal(const QString& notebook, const QString& path);
 };
